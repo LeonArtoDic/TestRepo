c7085098092dc16a792a3ec2ba8e947d5e9ee6c5
Artem leonov <artemleobiz@gmail.com>
2024-12-18 01:32:29 +0300

    Prototype 3

--- ScreenCheckerPrototype/ScreenCheckerPrototype/Intents/ShortcutsURL.swift
+++ ScreenCheckerPrototype/ScreenCheckerPrototype/Intents/ShortcutsURL.swift
@@ -2,11 +2,14 @@ import Foundation
 
 enum Shortcuts {
     case checkPhoneNumber
+    case extractorPhoneNumbers
     
     var url: URL? {
         switch self {
         case .checkPhoneNumber:
             URL(string: "https://www.icloud.com/shortcuts/7d976ed28a78492db72c24a26d93e3d8")
+        case .extractorPhoneNumbers:
+            URL(string: "https://www.icloud.com/shortcuts/ab7a257d8eb145a8af7ac6c7a2a75e3e")
         }
     }
 }
--- ScreenCheckerPrototype/ScreenCheckerPrototype/ViewController.swift
+++ ScreenCheckerPrototype/ScreenCheckerPrototype/ViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let callManager = CallManager()
+    lazy var providerDelegate = ProviderDelegate(callManager: callManager)
+    
     let mainButton = UIButton()
-
+    let callMeButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,14 +23,31 @@ class ViewController: UIViewController {
         mainButton.center = view.center
         mainButton.addTarget(self, action: #selector(mainButtonTapped), for: .touchUpInside)
         view.addSubview(mainButton)
+        
+        callMeButton.setTitle("Call me", for: .normal)
+        callMeButton.titleLabel?.font = .systemFont(ofSize: 25, weight: .bold)
+        callMeButton.bounds.size = CGSize(width: 170, height: 50)
+        callMeButton.backgroundColor = .orange
+        callMeButton.layer.cornerRadius = 12
+        callMeButton.center = view.center
+        callMeButton.frame.origin.y += 80
+        callMeButton.addTarget(self, action: #selector(callMeButtonTapped), for: .touchUpInside)
+        view.addSubview(callMeButton)
     }
     
     @objc func mainButtonTapped() {
-        guard let url = Shortcuts.checkPhoneNumber.url else { return }
+//        guard let url = Shortcuts.checkPhoneNumber.url else { return } main Shortcut
+        guard let url = Shortcuts.extractorPhoneNumbers.url else { return } // for test
         
         UIApplication.shared.canOpenURL(url)
         ? UIApplication.shared.open(url, options: [:])
         : print("Cannot open URL")
+    }
+    
+    @objc func callMeButtonTapped() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.providerDelegate.reportIncomingCall(uuid: UUID(), handle: "+375336698765", completion: nil)
+        }
     }
 }
 
